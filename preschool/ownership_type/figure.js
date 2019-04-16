0aa09f55d0e7495facc2c53f1c83f81890d5cc7f
et4m1r <et4m1r@gmail.com>
2019-04-16 10:17:32 +0800

    Prescholl updated

---
+++ preschool/ownership_type/figure.js
@@ -0,0 +1,127 @@
+var figure = {
+    "data": [
+        {
+            "mode": "markers",
+            "name": "Public",
+            "type": "bar",
+            "xsrc": "et4m1r:1:a65106",
+            "x": [
+                "2009-2010",
+                "2010-2011",
+                "2011-2012",
+                "2012-2013",
+                "2013-2014",
+                "2014-2015",
+                "2015-2016",
+                "2016-2017",
+                "2017-2018",
+                "2018-2019"
+            ],
+            "ysrc": "et4m1r:1:36cec0",
+            "y": [
+                "712",
+                "722",
+                "734",
+                "750",
+                "764",
+                "777",
+                "826",
+                "854",
+                "878",
+                "889"
+            ],
+            "textsrc": "et4m1r:1:a65106",
+            "text": [
+                "2009-2010",
+                "2010-2011",
+                "2011-2012",
+                "2012-2013",
+                "2013-2014",
+                "2014-2015",
+                "2015-2016",
+                "2016-2017",
+                "2017-2018",
+                "2018-2019"
+            ],
+            "hoverinfo": "x+y+text+name",
+            "orientation": "v",
+            "textposition": "none"
+        },
+        {
+            "name": "Private",
+            "type": "bar",
+            "xsrc": "et4m1r:1:a65106",
+            "x": [
+                "2009-2010",
+                "2010-2011",
+                "2011-2012",
+                "2012-2013",
+                "2013-2014",
+                "2014-2015",
+                "2015-2016",
+                "2016-2017",
+                "2017-2018",
+                "2018-2019"
+            ],
+            "ysrc": "et4m1r:1:1a0971",
+            "y": [
+                "102",
+                "117",
+                "145",
+                "195",
+                "303",
+                "394",
+                "462",
+                "500",
+                "538",
+                "546"
+            ],
+            "hoverinfo": "x+y+name",
+            "showlegend": true,
+            "orientation": "v",
+            "textposition": "outside"
+        }
+    ],
+    "layout": {
+        "font": {
+            "size": 10
+        },
+        "title": {
+            "text": "Click to enter Plot title"
+        },
+        "xaxis": {
+            "type": "category",
+            "range": [
+                -0.5,
+                9.5
+            ],
+            "title": {
+                "text": "Click to enter X axis title"
+            },
+            "autorange": true,
+            "tickangle": "auto",
+            "showticklabels": true
+        },
+        "yaxis": {
+            "type": "linear",
+            "range": [
+                0,
+                935.7894736842105
+            ],
+            "title": {
+                "text": "Click to enter Y axis title"
+            },
+            "autorange": true
+        },
+        "bargap": 0.19999999999999996,
+        "images": [],
+        "shapes": [],
+        "modebar": {
+            "orientation": "h"
+        },
+        "autosize": true,
+        "annotations": [],
+        "bargroupgap": 0.1
+    },
+    "frames": []
+}
